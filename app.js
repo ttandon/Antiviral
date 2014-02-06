3663a89ff339836202afb7ff987113b4cbb39c5f
Stuart Updegrave <stuart@updegrave.com>
2014-02-06 15:15:48 +0100

    refactor inoculation as a result of selecting higher wrapper node

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ var blacklist          = [ "viralnova.com", "upworthy.com", "buzzfeed.com", "res
     blacklistRegex     = new RegExp(blacklist.join('|'), 'i'), 
     timeline           = document.querySelector('div[id^=topnews_main_stream_]'), 
     MutationObserver   = MutationObserver || WebKitMutationObserver,
-    removedNodeMessage = "AntiViral removed a post containing the blocked site ",
+    removedNodeMessage = "AntiViral removed a post containing the blocked site",
     removeWrapper      = false,
     debugMode          = true,
-    // contentSelector    = "div.clearfix.userContentWrapper",
-    contentSelector    = "div[data-timestamp]", // different selector 
+    contentSelector    = "div[data-ft*=mf_story_key]", 
     commentSelector    = "div.UFICommentContent",
     Settings           = {
       ExecutionMode: null,
@@ -16,87 +15,63 @@ var blacklist          = [ "viralnova.com", "upworthy.com", "buzzfeed.com", "res
     };
 
 function Antiviralize() {
+  var wrapper; 
   [].slice
     // get all external links 
     .call(timeline.querySelectorAll('a[target=_blank]'))
     // filter to only process links on blacklist
     .filter(function(e){ return !!e.href.match(blacklistRegex); })
-    .forEach( function(link) { 
-      var wrapper, 
-          // outerWrapper, 
-          parent, 
-          grandparent;
-      
-      // make sure the link isn't in a comment
-      if (!ancestor(link, commentSelector)) {
-        wrapper = ancestor(link, contentSelector);
-   
-        // // check for nested userContentWrapper nodes
-        // if (outerWrapper = ancestor(wrapper, contentSelector)) { 
-        //   debug("found outerwrapper");
-        //   wrapper = outerWrapper; 
-        // }
+    .forEach( function(link) {       
+      if (!ancestor(link, commentSelector) && (wrapper = ancestor(link, contentSelector))) {
+        if (Settings.ExecutionMode === Settings.REMOVE && (!inoculated(wrapper.parentNode))) {
+          markAsInoculated(wrapper.parentNode);
+          wrapper.parentNode.removeChild(wrapper);
+        }
+        else if (!inoculated(wrapper)) {
+          markAsInoculated(wrapper);
 
-        if (wrapper) {
-          if (Settings.ExecutionMode === Settings.REMOVE
-              && (parent = wrapper.parentNode) 
-              && (grandparent = parent.parentNode)) {
-            inoculate(grandparent, parent, link);
+          if (Settings.ExecutionMode === Settings.DEBUG) { 
+            wrapper.firstChild.style.display = 'none';
+          } 
+          else { 
+            wrapper.removeChild(wrapper.firstChild);  
           }
-          else if ((parent = wrapper.parentNode) && !inoculated(parent)) {
-            inoculate(parent, wrapper, link);
-          }    
+
+          reportInoculation(link, wrapper); 
         }
       }
   });
 }
 
 function ancestor(elem, selector) {
-  var matchesSelector = elem.matches || elem.webkitMatchesSelector || elem.mozMatchesSelector || elem.msMatchesSelector;
-  elem = elem.parentNode;
-  
-  while (elem && elem !== document) {
-    if (matchesSelector.bind(elem)(selector)) { 
-      return elem;
-    } 
-    else {
-      elem = elem.parentNode;
+  if (elem) {
+    var matchesSelector = elem.matches || elem.webkitMatchesSelector || elem.mozMatchesSelector || elem.msMatchesSelector;
+    elem = elem.parentNode;
+
+    while (elem && elem !== document) {
+      if (matchesSelector.bind(elem)(selector)) { 
+        return elem;
+      } 
+      else {
+        elem = elem.parentNode;
+      }
     }
   }
-  
-  if (!elem) { debug(arguments); }
   return null;
 }
 
-function inoculate(parent, child, link) {
-  if (Settings.ExecutionMode === Settings.DEBUG) { 
-    child.style.display = 'none';
-  } 
-  else { 
-    parent.removeChild(child);  
-  }
-  
-  markAsInoculated(parent);
-  
-  if (link) {
-    reportInoculation(link, parent); 
-  }
-}
-
 function reportInoculation(link, parent) {
-  var report = [removedNodeMessage, link.href.match(blacklistRegex)[0], '.'];
+  var report = [removedNodeMessage, link.href.match(blacklistRegex)[0]];
   
-  // debug(report.join(''));
-
   if (Settings.ExecutionMode === Settings.DEBUG) {
-    // debug(link);
-    report.push(" <a class='antiviral restore'>Restore</a>");
+    debug(report.join(' '));
+    report.push("<a onclick='function(e) { alert(e); }' class='antiviral restore'>Restore</a>");
     var node = document.createElement('span');
     parent.appendChild(node);
-    node.innerHTML = report.join('');
+    node.innerHTML = report.join(' ');
   }
   else {
-    parent.innerHTML = report.join('');
+    parent.innerHTML = report.join(' ');
   }
 }
 
@@ -120,7 +95,6 @@ function debug() {
     });
   }
 }
-
 
 ( function() { 
     Settings.ExecutionMode = debugMode 
